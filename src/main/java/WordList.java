fbba67026804ed8ffd259654a90b4521f1d63939
Minty2000 <142567186+Minty2000@users.noreply.github.com>
2024-04-03 12:11:31 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,24 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    int c =0;
+    for (int i =0 ; i < myList.size();i++){
+      if (myList.get(i).length == len ){
+        c++;
+    }
+    return c;
+  }
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
+    for(int i = 0;i < myList.size();i++){
+    if (myList.get(i).length == length){
+      myList.remove(i);
+    }
   }
+
+  
+}
 }
